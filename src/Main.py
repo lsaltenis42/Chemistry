c94cc7c1a41d37f9311f8af3daf2a7d0befcdd41
Lukas Saltenis <tl00xu@student.aau.dk>
2023-12-26 17:13:49 +0100

    minor change

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,6 @@ import sys
 import os
 
 current_path = os.getcwd()
-sys.path.insert(0,f'{current_path}\\objects')
+sys.path.insert(0,f'{current_path}//objects')
 from example import fib
 print(fib(4))
