94b557d9af4312ebee9f2e68d833a8a17a3ff3b8
AndriusPalubinskas <119738730+AndriusPalubinskas@users.noreply.github.com>
2023-12-26 23:46:26 +0100

    tkinter test

--- src/draw_compound.py
+++ src/draw_compound.py
@@ -1,4 +1,13 @@
 import tkinter as tk
-from tkinter import ttk
-
 window = tk.Tk()
+window.title("Window")
+width = 800
+heigth = 800
+
+window.geometry(f"{width}x{heigth}")
+
+e = tk.Entry(window).anchor("center")
+e = tk.Entry(window).location(width/2, heigth/2)
+
+
+window.mainloop()
