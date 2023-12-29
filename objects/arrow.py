4d9c68c5f58b227c805e9c4871738b38d2ebbc80
Lukas Saltenis <tl00xu@student.aau.dk>
2023-12-29 11:42:38 +0100

    Progress

--- objects/arrow.py
+++ objects/arrow.py
@@ -1,10 +1,33 @@
 from enum import Enum
-    
+   
 class arrow:
-    arrow_types = ["--><-","-><--","-><-","->","<-"]
+    placement = {"before" : 0, "after": 1}
+    arrow_types = [{"style": "--><-", "length": 5},
+                     {"style": "-><--", "length": 5},
+                     {"style": "-><-", "length": 4},
+                     {"style": "->", "length": 2},
+                     {"style": "<-", "length": 2}]
+    #arrow_types = [{"first_part":"-->", "last_part": "<-", "caption_start_index": 3, "lenght_after_caption": 2},
+     #              {"first_part":"->", "last_part": "<--", "caption_start_index": 2, "lenght_after_caption": 3},
+    #               {"first_part":"->", "last_part": "<-", "caption_start_index": 2, "lenght_after_caption": 2},
+    #               {"first_part":"-", "last_part": ">", "caption_start_index": 1, "lenght_after_caption": 1},
+     #              {"first_part":"<", "last_part": "-", "caption_start_index": 1, "lenght_after_caption": 1}]
 
-    def __init__(self, arrow_type):
+    def __init__(self, arrow_type, index, caption_before = "", caption_after = ""):
         self.type = arrow_type
+        self.caption = {"before": caption_before,
+                         "after": caption_after}
+        self.index = index
 
+    def caption_index_range(self):
+        before_start_index = self.index - len(self.caption["before"])
+        before_end_index = self.index
+        after_start_index = self.index + len(self.type)
+        after_end_index = after_start_index + len(self.caption["after"])
+        return {"before":{"start_index": before_start_index, "end_index": before_end_index}, "after":{"start_index": after_start_index, "end_index": after_end_index}}
 
-
+    def total_length(self): # This length includes the arrow and its captions
+        caption_range = self.caption_index_range()
+        before_caption_length = caption_range["before"]["end_index"] - caption_range["before"]["start_index"]
+        after_caption_length = caption_range["afer"]["end_index"] - caption_range["after"]["start_index"]
+        return before_caption_length + len(self.type) + after_caption_length
--- src/user_input.py
+++ src/user_input.py
@@ -1,5 +1,9 @@
+from dataclasses import replace
 import os
 import sys
+from unittest.util import unorderable_list_difference
+
+#from macpath import split
 
 #Makes contents of objects file accessible
 current_path = os.getcwd()
@@ -11,11 +15,172 @@ from linker import linker
 from molecule import molecule
 from reaction import reaction
 
+def store_arrows(user_input, arrows):
+    for potential_arrow in arrow.arrow_types:
+        arrow_start_index = 0
+        occurrences = user_input.count(potential_arrow["style"])
+        for _ in range(occurrences):
+            arrow_start_index = user_input.index(potential_arrow["style"])
+            arrow_end_index = arrow_start_index + potential_arrow["length"]-1
+            before_caption = ""
+            after_caption = ""
+            
+            if user_input[arrow_start_index-1] == "\"": #Checks if caption exists before arrow
+                sub_string = user_input[:arrow_start_index-1]
+                sub_string = sub_string[::-1] #sub_string is reversed
+                second_quotation_mark_index = arrow_start_index - 1 - sub_string.index("\"") - 1
+                before_caption = user_input[second_quotation_mark_index:arrow_start_index]
+                arrow_start_index = second_quotation_mark_index
+
+            if arrow_end_index < len(user_input) and user_input[arrow_end_index+1] == "\"": #Checks if caption exists after arrow
+                sub_string = user_input[arrow_end_index+2:]
+                second_quotation_mark_index = arrow_end_index+2 + sub_string.index("\"")
+                after_caption = user_input[arrow_end_index+1:second_quotation_mark_index]
+                arrow_end_index = second_quotation_mark_index
+
+            arrow_to_append = arrow(potential_arrow["style"], arrow_start_index, before_caption, after_caption)
+            arrows.append(arrow_to_append)
+            configured_substring = user_input[arrow_start_index:arrow_end_index].replace(user_input[arrow_start_index:arrow_end_index],"$")
+            user_input = user_input[:arrow_start_index] + configured_substring + user_input[arrow_end_index:]
+            
+            
+
+    for i, item in enumerate(arrows):
+        for j in arrows:
+            caption_range = j.caption_index_range()
+            if (item.index > caption_range["before"]["start_index"] and item.index < caption_range["before"]["end_index"]) or (item.index > caption_range["after"]["start_index"] and item.index < caption_range["after"]["end_index"]):
+                arrows.pop(i)
+    
+    for n in arrows:
+        print(n.type)
+        print(n.index)
+        print(n.caption["before"])
+        print(n.caption["after"])
+    
+    return user_input
+"""
+def store_arrows(user_input, arrows):
+    for potential_arrow in arrow.arrow_types:
+        arrow_start_index = 0
+        occurrences = user_input.count(potential_arrow["first_part"])
+        if occurrences != 0:
+            for _ in range(occurrences):
+                arrow_start_index = user_input[arrow_start_index:].index(potential_arrow["first_part"])
+                caption_start_index = arrow_start_index+potential_arrow["caption_start_index"]
+                try:
+                    caption_end_index = user_input.index(potential_arrow["last_part"],arrow_start_index)
+                except :
+                    continue
+                
+                arrow_type = potential_arrow["first_part"]+potential_arrow["last_part"]
+                caption = user_input[caption_start_index:caption_end_index].strip()
+                current_arrow = arrow(arrow_type, caption, arrow_start_index)
+                arrow_end_index = caption_end_index + potential_arrow["lenght_after_caption"]
+                arrow_length = arrow_end_index - arrow_start_index
+                try:
+                    store_arrows(user_input[caption_start_index:caption_end_index], arrows).index("@")
+                except:
+                    user_input = user_input.replace(user_input[arrow_start_index:arrow_end_index], "@") 
+                    arrows.append(current_arrow)
+                
+                
+    arrows.sort(key = lambda element:element.index)
+
+    return user_input
+
+def store_linkers_and_molecules(user_input, reaction_scheme, arrows):
+    i = 0
+    user_input_length = len(user_input)
+    while i < user_input_length:
+        current_char = user_input[i]
+        if current_char in linker.linker_types or current_char == '@':
+            user_input = user_input[:i] + '!' + user_input[i:]
+            user_input = user_input[:i+2] + '!' + user_input[i+2:]
+            i += 3
+            user_input_length += 2
+        else: i += 1
+
+    
+    print(user_input)
+    user_input = user_input.strip('!')
+    componenents = user_input.split("!")
+
+    for component in componenents:
+        if component in linker.linker_types:
+           linker_to_append = linker(component)
+           reaction_scheme.append(linker_to_append)
+        elif component == '@':
+            arrow_to_append = arrows[0]
+            reaction_scheme.append(arrow_to_append)
+            arrows.pop(0)
+        else:
+            if is_organic(component): 
+                #Organic script or alternatively, 
+                #send the name to the object, 
+                #which processes the moleculer
+                reaction_scheme.append(molecule(str(component), "organic", None))
+            else:
+                marked_character = ""
+                atoms = []
+                for character in component: 
+                    
+                    if character.isupper():
+                        marked_character += "@" + character
+                    else:
+                        marked_character += character
+                        atoms = marked_character.split("@")
+                
+                reaction_scheme.append(molecule(str(component), "inorganic", atoms))
+
+    print(reaction_scheme)
+    """
+
+
+"""                      
+def configure_input(user_input):
+    occurrences = user_input.count("\"")
+    if occurrences % 2 != 0:
+        print("Invalid input!")
+    elif occurrences != 0:
+        indices = [0]
+        current_index = -1
+        configured_input = user_input[:]
+
+        for _ in range(occurrences):
+            current_index = user_input.index("\"")
+            indices.append(current_index)
+            user_input = user_input.replace("\"","!", 1)
+
+        indices.append(len(user_input))
+
+        for i in range(int(len(indices)/2)):
+            sub_string = user_input[indices[2*i]:indices[2*i+1]]
+            configured_input = user_input.replace(sub_string,sub_string.replace(" ","")) 
+            
+            
+    else: user_input = user_input.replace(" ","")
+    print(user_input)
+    """    
 #Processes input into a list of objects 
 def get_input():
+    arrows = []
     reaction_scheme = []
 
-    user_input = str(input("enter a reaction"))
+    user_input = str(input("enter a reaction")).replace("\\\"","@")
+    if user_input.count("\"") % 2 != 0:
+        print("invalid input")
+
+    #configure_input(user_input)
+    user_input = store_arrows(user_input, arrows)
+    #store_linkers_and_molecules(user_input, reaction_scheme, arrows)
+    
+        
+
+            
+                
+
+
+    """
     reagents = user_input.split(" ")
 
     for reagent in reagents:
@@ -53,9 +218,9 @@ def get_input():
                         atoms.remove("")
                 
                 reaction_scheme.append(molecule(str(reagent), "inorganic", atoms))
-    #print(reaction_scheme[3].type)
+    print(reaction_scheme)
 
-
+"""
 #Determines whether molecule is organic based on length-criteria
 #Assumes input is given in symbolic form, i.e. CO_2 not carbondioxide
 
