e1017294113f5b1b104ab0831c4edc7fe515c957
AndriusPalubinskas <119738730+AndriusPalubinskas@users.noreply.github.com>
2024-02-02 14:34:29 +0100

    Works

--- javascript/01-Experimental.js
+++ javascript/01-Experimental.js
@@ -1,22 +1,28 @@
-
-function Rectangle(length, width) {
-    this.length;
+function Rectangle(width, height) {
     this.width;
+    this.height;
 
     this.draw = function () {
-        console.log("|");
-        console.log("-".repeat(this.width));
-        console.log("|");
-        for (let i = 0; i < this.length; i++){
-            console.log("|");
-            " ".repeat(this.width);
-            console.log("|")
+        if (width < 1 || height < 1){
+            console.log("Invalid dimensions!")
+            return
+        } else {
+            console.log("|" + "\u203E".repeat(width) + "|")
+            for (let i = 0; i < height - 2; i++){
+                console.log("|" + " ".repeat(width) + "|");
+            }
+            if (height == 1){
+                console.log(" " + "\u203E".repeat(width) + " ")
+            } else {
+                console.log("|" + "\u005F".repeat(width) + "|")
+            }
+            
         }
-        console.log("|");
-        console.log("-".repeat(this.width));
-        console.log("|");
     }
 }
 
-const Rectangle1 = new Rectangle(5, 10);
+const Rectangle1 = new Rectangle(1, 1);
+const Rectangle2 = new Rectangle(2, 2);
+
 Rectangle1.draw();
+Rectangle2.draw();
