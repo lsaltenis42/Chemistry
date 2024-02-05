61bfe150ecb8471b67c7c36086cd7693e3566c6e
AndriusPalubinskas <119738730+AndriusPalubinskas@users.noreply.github.com>
2024-02-05 15:00:43 +0100

    p3

---
+++ .vs/ProjectEuler/p3.js
@@ -0,0 +1,17 @@
+function getPrime(n){
+    let primes = []; 
+    let i = 1;
+    let nInput = n; 
+
+    while (n > 1 && i <= n) {
+        while (n % i == 0) {
+            primes.push(i);
+            n = n/i;
+            i ++
+        }
+        i ++
+    }
+    return primes[primes.length-1];
+}
+
+console.log(getPrime(600851475143)); 
