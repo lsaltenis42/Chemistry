23f2792ee3a9565d7692625c113e320f8e49a64a
AndriusPalubinskas <119738730+AndriusPalubinskas@users.noreply.github.com>
2024-02-09 15:22:35 +0100

    waesrdtf

---
+++ Playground/ProjectEuler/p6.js
@@ -0,0 +1,18 @@
+let number = 100; 
+let factors = [];
+
+for(let i = 0; i <= 100; i++){
+    factors.push(i);
+}
+
+
+function getProduct(x,y){
+    let product = []; 
+    for(let j = 0; j <= String(x).length;){
+
+    }
+    return product
+}
+
+
+console.log(String(12344).length);
